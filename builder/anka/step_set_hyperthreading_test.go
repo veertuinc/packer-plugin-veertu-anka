f92ee42a2acbe7827f64c1400e6d0886fec49216
Nathaniel Kierpiec <nate.kierpiec@gmail.com>
2021-02-22 18:00:37 -0500

    Add gomock library for testing client mock (#7)
    Easily test the logic of the client using gomock by having it implement a controller which handles how we can use it and a recorder which handles what is called when we test our functions.
    
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- builder/anka/step_set_hyperthreading_test.go
+++ builder/anka/step_set_hyperthreading_test.go
@@ -5,14 +5,21 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	"github.com/hashicorp/packer-plugin-sdk/packer"
 	c "github.com/veertuinc/packer-builder-veertu-anka/client"
-	"github.com/veertuinc/packer-builder-veertu-anka/testutils"
+	mocks "github.com/veertuinc/packer-builder-veertu-anka/mocks"
 	"gotest.tools/assert"
 )
 
 func TestRun(t *testing.T) {
+	// gomock implementation for testing the client
+	// used for tracking and asserting expectations
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish() // will run assertions at this point for our expectations
+	client := mocks.NewMockClient(mockCtrl)
+
 	step := StepSetHyperThreading{}
 	ui := packer.TestUi(t)
 	ctx := context.Background()
@@ -21,15 +28,7 @@ func TestRun(t *testing.T) {
 	state.Put("ui", ui)
 	state.Put("vm_name", "foo")
 
-	t.Run("test disabled or nil htt values", func(t *testing.T) {
-		expectedResults := make(map[string]c.MachineReadableOutput)
-		expectedErrors := make(map[string]error)
-
-		client := &testutils.TestClient{
-			Results: expectedResults,
-			Errors:  expectedErrors,
-		}
-
+	t.Run("disabled or nil htt values", func(t *testing.T) {
 		config := &Config{
 			EnableHtt:  false,
 			DisableHtt: false,
@@ -43,14 +42,6 @@ func TestRun(t *testing.T) {
 	})
 
 	t.Run("conflicting htt values", func(t *testing.T) {
-		expectedResults := make(map[string]c.MachineReadableOutput)
-		expectedErrors := make(map[string]error)
-
-		client := &testutils.TestClient{
-			Results: expectedResults,
-			Errors:  expectedErrors,
-		}
-
 		config := &Config{
 			EnableHtt:  true,
 			DisableHtt: true,
@@ -63,13 +54,31 @@ func TestRun(t *testing.T) {
 		assert.Equal(t, stepAction, multistep.ActionHalt)
 	})
 
-	t.Run("test enable htt when not configured", func(t *testing.T) {
-		expectedResults := make(map[string]c.MachineReadableOutput)
-		expectedErrors := make(map[string]error)
+	t.Run("configure htt", func(t *testing.T) {
+		config := &Config{
+			EnableHtt:  true,
+			DisableHtt: false,
+		}
 
-		client := &testutils.TestClient{
-			Results: expectedResults,
-			Errors:  expectedErrors,
+		state.Put("client", client)
+		state.Put("config", config)
+
+		gomock.InOrder(
+			client.EXPECT().Describe("foo").Return(c.DescribeResponse{}, nil).Times(1),
+			client.EXPECT().Show("foo").Return(c.ShowResponse{}, nil).Times(1),
+			client.EXPECT().Stop(c.StopParams{VMName: "foo", Force: true}).Return(nil).Times(1),
+			client.EXPECT().Modify("foo", "set", "cpu", "--htt").Return(nil).Times(1),
+		)
+
+		stepAction := step.Run(ctx, state)
+		assert.Equal(t, stepAction, multistep.ActionContinue)
+	})
+
+	t.Run("enable htt when already configured", func(t *testing.T) {
+		var describeResponse c.DescribeResponse
+		err := json.Unmarshal(json.RawMessage(`{"CPU": {"Threads": 2}}`), &describeResponse)
+		if err != nil {
+			t.Fail()
 		}
 
 		config := &Config{
@@ -80,67 +89,32 @@ func TestRun(t *testing.T) {
 		state.Put("client", client)
 		state.Put("config", config)
 
-		expectedResults["describe foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{}`),
-		}
-		expectedResults["show foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{}`),
-		}
-		expectedResults["stop --force foo"] = c.MachineReadableOutput{}
-		expectedResults["modify foo set cpu --htt"] = c.MachineReadableOutput{
-			Status: "OK",
-		}
-
-		expectedErrors["describe foo"] = nil
-		expectedErrors["show foo"] = nil
-		expectedErrors["stop --force foo"] = nil
-		expectedErrors["modify foo set cpu --htt"] = nil
+		client.EXPECT().Describe("foo").Return(describeResponse, nil).Times(1)
 
 		stepAction := step.Run(ctx, state)
-
-		assert.Equal(t, "describe foo", client.Commands[0])
-		assert.Equal(t, "show foo", client.Commands[1])
-		assert.Equal(t, "stop --force foo", client.Commands[2])
-		assert.Equal(t, "modify foo set cpu --htt", client.Commands[3])
 		assert.Equal(t, stepAction, multistep.ActionContinue)
 	})
 
-	t.Run("test enable htt when already configured", func(t *testing.T) {
-		expectedResults := make(map[string]c.MachineReadableOutput)
-		expectedErrors := make(map[string]error)
-
-		client := &testutils.TestClient{
-			Results: expectedResults,
-			Errors:  expectedErrors,
-		}
-
+	t.Run("disable htt when not configured", func(t *testing.T) {
 		config := &Config{
-			EnableHtt:  true,
-			DisableHtt: false,
+			EnableHtt:  false,
+			DisableHtt: true,
 		}
 
 		state.Put("client", client)
 		state.Put("config", config)
 
-		expectedResults["describe foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{"CPU": {"Threads": 2}}`),
-		}
-
-		expectedErrors["describe foo"] = nil
+		client.EXPECT().Describe("foo").Return(c.DescribeResponse{}, nil).Times(1)
 
 		stepAction := step.Run(ctx, state)
-
-		assert.Equal(t, "describe foo", client.Commands[0])
 		assert.Equal(t, stepAction, multistep.ActionContinue)
 	})
 
-	t.Run("test disable htt when its not configured", func(t *testing.T) {
-		expectedResults := make(map[string]c.MachineReadableOutput)
-		expectedErrors := make(map[string]error)
-
-		client := &testutils.TestClient{
-			Results: expectedResults,
-			Errors:  expectedErrors,
+	t.Run("disable htt", func(t *testing.T) {
+		var describeResponse c.DescribeResponse
+		err := json.Unmarshal(json.RawMessage(`{"CPU": {"Threads": 2}}`), &describeResponse)
+		if err != nil {
+			t.Fail()
 		}
 
 		config := &Config{
@@ -151,67 +125,22 @@ func TestRun(t *testing.T) {
 		state.Put("client", client)
 		state.Put("config", config)
 
-		expectedResults["describe foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{"CPU": {"Threads": 0}}`),
-		}
-
-		expectedErrors["describe foo"] = nil
-
-		stepAction := step.Run(ctx, state)
-
-		assert.Equal(t, "describe foo", client.Commands[0])
-		assert.Equal(t, stepAction, multistep.ActionContinue)
-	})
-
-	t.Run("test disable htt when its configured", func(t *testing.T) {
-		expectedResults := make(map[string]c.MachineReadableOutput)
-		expectedErrors := make(map[string]error)
-
-		client := &testutils.TestClient{
-			Results: expectedResults,
-			Errors:  expectedErrors,
-		}
-
-		config := &Config{
-			EnableHtt:  false,
-			DisableHtt: true,
-		}
-
-		state.Put("client", client)
-		state.Put("config", config)
-
-		expectedResults["describe foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{"CPU": {"Threads": 2}}`),
-		}
-		expectedResults["show foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{}`),
-		}
-		expectedResults["stop --force foo"] = c.MachineReadableOutput{}
-		expectedResults["modify foo set cpu --no-htt"] = c.MachineReadableOutput{
-			Status: "OK",
-		}
-
-		expectedErrors["describe foo"] = nil
-		expectedErrors["show foo"] = nil
-		expectedErrors["stop --force foo"] = nil
-		expectedErrors["modify foo set cpu --no-htt"] = nil
+		gomock.InOrder(
+			client.EXPECT().Describe("foo").Return(describeResponse, nil).Times(1),
+			client.EXPECT().Show("foo").Return(c.ShowResponse{}, nil).Times(1),
+			client.EXPECT().Stop(c.StopParams{VMName: "foo", Force: true}).Return(nil).Times(1),
+			client.EXPECT().Modify("foo", "set", "cpu", "--no-htt").Return(nil).Times(1),
+		)
 
 		stepAction := step.Run(ctx, state)
-
-		assert.Equal(t, "describe foo", client.Commands[0])
-		assert.Equal(t, "show foo", client.Commands[1])
-		assert.Equal(t, "stop --force foo", client.Commands[2])
-		assert.Equal(t, "modify foo set cpu --no-htt", client.Commands[3])
 		assert.Equal(t, stepAction, multistep.ActionContinue)
 	})
 
 	t.Run("test rerun when vm is currently running", func(t *testing.T) {
-		expectedResults := make(map[string]c.MachineReadableOutput)
-		expectedErrors := make(map[string]error)
-
-		client := &testutils.TestClient{
-			Results: expectedResults,
-			Errors:  expectedErrors,
+		var showResponse c.ShowResponse
+		err := json.Unmarshal(json.RawMessage(`{ "Status": "running" }`), &showResponse)
+		if err != nil {
+			t.Fail()
 		}
 
 		config := &Config{
@@ -222,31 +151,15 @@ func TestRun(t *testing.T) {
 		state.Put("client", client)
 		state.Put("config", config)
 
-		expectedResults["describe foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{}`),
-		}
-		expectedResults["show foo"] = c.MachineReadableOutput{
-			Body: json.RawMessage(`{ "Status": "running" }`),
-		}
-		expectedResults["stop --force foo"] = c.MachineReadableOutput{}
-		expectedResults["modify foo set cpu --no-htt"] = c.MachineReadableOutput{
-			Status: "OK",
-		}
-		expectedResults["start foo"] = c.MachineReadableOutput{}
-
-		expectedErrors["describe foo"] = nil
-		expectedErrors["show foo"] = nil
-		expectedErrors["stop --force foo"] = nil
-		expectedErrors["modify foo set cpu --no-htt"] = nil
-		expectedErrors["start foo"] = nil
+		gomock.InOrder(
+			client.EXPECT().Describe("foo").Return(c.DescribeResponse{}, nil).Times(1),
+			client.EXPECT().Show("foo").Return(showResponse, nil).Times(1),
+			client.EXPECT().Stop(c.StopParams{VMName: "foo", Force: true}).Return(nil).Times(1),
+			client.EXPECT().Modify("foo", "set", "cpu", "--htt").Return(nil).Times(1),
+			client.EXPECT().Start(c.StartParams{VMName: "foo"}).Return(nil).Times(1),
+		)
 
 		stepAction := step.Run(ctx, state)
-
-		assert.Equal(t, "describe foo", client.Commands[0])
-		assert.Equal(t, "show foo", client.Commands[1])
-		assert.Equal(t, "stop --force foo", client.Commands[2])
-		assert.Equal(t, "modify foo set cpu --htt", client.Commands[3])
-		assert.Equal(t, "start foo", client.Commands[4])
 		assert.Equal(t, stepAction, multistep.ActionContinue)
 	})
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/assert v0.0.0-20170929043011-405dbfeb8e38
 	github.com/alecthomas/colour v0.1.0 // indirect
 	github.com/alecthomas/repr v0.0.0-20201120212035-bb82daffcca2 // indirect
+	github.com/golang/mock v1.5.0
 	github.com/groob/plist v0.0.0-20200425180238-0f631f258c01
 	github.com/hashicorp/hcl/v2 v2.8.2
 	github.com/hashicorp/packer v1.6.7-0.20210126105722-aef4ced967ec
