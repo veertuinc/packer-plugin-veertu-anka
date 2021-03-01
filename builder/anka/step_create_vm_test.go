37795b64f4c9f04cd5aba5f45a263a30fd866308
Nathaniel Kierpiec <nkierpiec@chef.io>
2021-03-01 17:44:48 -0500

    Add additional test coverage for create vm logic
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- builder/anka/step_create_vm_test.go
+++ builder/anka/step_create_vm_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -14,10 +15,6 @@ import (
 	mocks "github.com/veertuinc/packer-builder-veertu-anka/mocks"
 	"gotest.tools/assert"
 )
-
-//
-//	TODO: Need to add more tests for modifying VM Properties around port forwarding rules
-//
 
 func TestCreateVMRun(t *testing.T) {
 	// gomock implementation for testing the client
@@ -272,6 +269,271 @@ func TestCreateVMRun(t *testing.T) {
 		assert.Equal(t, mockui.SayMessages[0].Message, "Cloning source VM source_foo into a new virtual machine: foo")
 		assert.Equal(t, mockui.SayMessages[1].Message, "Modifying VM foo disk size to 120G")
 		assert.Equal(t, mockui.SayMessages[2].Message, "Modifying VM foo RAM to 16G")
+		assert.Equal(t, multistep.ActionContinue, stepAction)
+	})
+
+	t.Run("create vm with modify vm properties", func(t *testing.T) {
+		var sourceShowResponse c.ShowResponse
+		err := json.Unmarshal(json.RawMessage(`{ "UUID": "123456" }`), &sourceShowResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var clonedShowResponse c.ShowResponse
+		err = json.Unmarshal(json.RawMessage(`{ "Name": "foo", "UUID": "123456" }`), &clonedShowResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var clonedDescribeResponse c.DescribeResponse
+		err = json.Unmarshal(json.RawMessage(`{  }`), &clonedDescribeResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var config Config
+		err = json.Unmarshal(json.RawMessage(`
+			{
+				"PortForwardingRules": [
+					{
+						"PortForwardingGuestPort": 8080,
+						"PortForwardingHostPort": 80,
+						"PortForwardingRuleName": "rule1"
+					}
+				],
+				"SourceVMName": "source_foo",
+				"VMName": "foo",
+				"HWUUID": "abcdefgh"
+			}
+		`), &config)
+		if err != nil {
+			t.Fail()
+		}
+
+		state.Put("config", &config)
+		state.Put("client", client)
+
+		cloneParams := c.CloneParams{
+			VMName:     config.VMName,
+			SourceUUID: sourceShowResponse.UUID,
+		}
+
+		stopParams := c.StopParams{
+			VMName: clonedShowResponse.Name,
+			Force:  true,
+		}
+
+		gomock.InOrder(
+			client.EXPECT().Exists(config.SourceVMName).Return(true, nil).Times(1),
+			client.EXPECT().Show(config.SourceVMName).Return(sourceShowResponse, nil).Times(1),
+			client.EXPECT().Exists(config.VMName).Return(false, nil).Times(1),
+			client.EXPECT().Clone(cloneParams).Return(nil).Times(1),
+			client.EXPECT().Show(config.VMName).Return(clonedShowResponse, nil).Times(1),
+			client.EXPECT().Describe(config.VMName).Return(c.DescribeResponse{}, nil).Times(1),
+			client.EXPECT().Stop(stopParams).Return(nil).Times(1),
+			client.EXPECT().Modify(clonedShowResponse.Name, "add", "port-forwarding", "--host-port", strconv.Itoa(config.PortForwardingRules[0].PortForwardingHostPort), "--guest-port", strconv.Itoa(config.PortForwardingRules[0].PortForwardingGuestPort), "rule1").Return(nil).Times(1),
+			client.EXPECT().Stop(stopParams).Return(nil).Times(1),
+			client.EXPECT().Modify(clonedShowResponse.Name, "set", "custom-variable", "hw.UUID", config.HWUUID).Return(nil).Times(1),
+		)
+
+		mockui := packer.MockUi{}
+		mockui.Say(fmt.Sprintf("Cloning source VM %s into a new virtual machine: %s", config.SourceVMName, config.VMName))
+		mockui.Say(fmt.Sprintf("Ensuring %s port-forwarding (Guest Port: %s, Host Port: %s, Rule Name: %s)", clonedShowResponse.Name, strconv.Itoa(config.PortForwardingRules[0].PortForwardingGuestPort), strconv.Itoa(config.PortForwardingRules[0].PortForwardingHostPort), config.PortForwardingRules[0].PortForwardingRuleName))
+		mockui.Say(fmt.Sprintf("Modifying VM custom-variable hw.UUID to %s", config.HWUUID))
+
+		state.Put("vm_name", config.VMName)
+
+		stepAction := step.Run(ctx, state)
+		assert.Equal(t, mockui.SayMessages[0].Message, "Cloning source VM source_foo into a new virtual machine: foo")
+		assert.Equal(t, mockui.SayMessages[1].Message, "Ensuring foo port-forwarding (Guest Port: 8080, Host Port: 80, Rule Name: rule1)")
+		assert.Equal(t, mockui.SayMessages[2].Message, "Modifying VM custom-variable hw.UUID to abcdefgh")
+		assert.Equal(t, multistep.ActionContinue, stepAction)
+	})
+
+	t.Run("create vm with modify vm properties with already enabled port forwarding rules", func(t *testing.T) {
+		var sourceShowResponse c.ShowResponse
+		err := json.Unmarshal(json.RawMessage(`{ "UUID": "123456" }`), &sourceShowResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var clonedShowResponse c.ShowResponse
+		err = json.Unmarshal(json.RawMessage(`{ "Name": "foo", "UUID": "123456" }`), &clonedShowResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var clonedDescribeResponse c.DescribeResponse
+		err = json.Unmarshal(json.RawMessage(`
+			{
+				"network_cards": [
+					{
+						"port_forwarding_rules": [
+							{
+								"guest_port": 8080,
+								"host_port": 80,
+								"rule_name": "rule1"
+							}
+						]
+					}
+				]
+			}
+		`), &clonedDescribeResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var config Config
+		err = json.Unmarshal(json.RawMessage(`
+			{
+				"PortForwardingRules": [
+					{
+						"PortForwardingGuestPort": 8080,
+						"PortForwardingHostPort": 80,
+						"PortForwardingRuleName": "rule1"
+					}
+				],
+				"SourceVMName": "source_foo",
+				"VMName": "foo"
+			}
+		`), &config)
+		if err != nil {
+			t.Fail()
+		}
+
+		state.Put("config", &config)
+		state.Put("client", client)
+
+		cloneParams := c.CloneParams{
+			VMName:     config.VMName,
+			SourceUUID: sourceShowResponse.UUID,
+		}
+
+		gomock.InOrder(
+			client.EXPECT().Exists(config.SourceVMName).Return(true, nil).Times(1),
+			client.EXPECT().Show(config.SourceVMName).Return(sourceShowResponse, nil).Times(1),
+			client.EXPECT().Exists(config.VMName).Return(false, nil).Times(1),
+			client.EXPECT().Clone(cloneParams).Return(nil).Times(1),
+			client.EXPECT().Show(config.VMName).Return(clonedShowResponse, nil).Times(1),
+			client.EXPECT().Describe(config.VMName).Return(clonedDescribeResponse, nil).Times(1),
+		)
+
+		mockui := packer.MockUi{}
+		mockui.Say(fmt.Sprintf("Cloning source VM %s into a new virtual machine: %s", config.SourceVMName, config.VMName))
+		mockui.Say(fmt.Sprintf("Ensuring %s port-forwarding (Guest Port: %s, Host Port: %s, Rule Name: %s)", clonedShowResponse.Name, strconv.Itoa(config.PortForwardingRules[0].PortForwardingGuestPort), strconv.Itoa(config.PortForwardingRules[0].PortForwardingHostPort), config.PortForwardingRules[0].PortForwardingRuleName))
+		mockui.Error(fmt.Sprintf("Found an existing host port rule (%s)! Skipping without setting...", strconv.Itoa(config.PortForwardingRules[0].PortForwardingHostPort)))
+
+		state.Put("vm_name", config.VMName)
+
+		stepAction := step.Run(ctx, state)
+		assert.Equal(t, mockui.SayMessages[0].Message, "Cloning source VM source_foo into a new virtual machine: foo")
+		assert.Equal(t, mockui.SayMessages[1].Message, "Ensuring foo port-forwarding (Guest Port: 8080, Host Port: 80, Rule Name: rule1)")
+		assert.Equal(t, mockui.ErrorCalled, true)
+		assert.Equal(t, mockui.ErrorMessage, "Found an existing host port rule (80)! Skipping without setting...")
+		assert.Equal(t, multistep.ActionContinue, stepAction)
+	})
+
+	t.Run("create vm with both modify resources and properties", func(t *testing.T) {
+		var sourceShowResponse c.ShowResponse
+		err := json.Unmarshal(json.RawMessage(`{ "UUID": "123456" }`), &sourceShowResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var clonedShowResponse c.ShowResponse
+		err = json.Unmarshal(json.RawMessage(`{ "Name": "foo", "CPUCores": 8, "HardDrive": 40, "RAM": "8G", "UUID": "123456" }`), &clonedShowResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var clonedDescribeResponse c.DescribeResponse
+		err = json.Unmarshal(json.RawMessage(`{  }`), &clonedDescribeResponse)
+		if err != nil {
+			t.Fail()
+		}
+
+		var config Config
+		err = json.Unmarshal(json.RawMessage(`
+			{
+				"CPUCount": "4",
+				"DiskSize": "120G",
+				"RAMSize": "16G",
+				"PortForwardingRules": [
+					{
+						"PortForwardingGuestPort": 8080,
+						"PortForwardingHostPort": 80,
+						"PortForwardingRuleName": "rule1"
+					}
+				],
+				"SourceVMName": "source_foo",
+				"VMName": "foo",
+				"HWUUID": "abcdefgh"
+			}
+		`), &config)
+		if err != nil {
+			t.Fail()
+		}
+
+		state.Put("config", &config)
+		state.Put("client", client)
+
+		cloneParams := c.CloneParams{
+			VMName:     config.VMName,
+			SourceUUID: sourceShowResponse.UUID,
+		}
+
+		stopParams := c.StopParams{
+			VMName: clonedShowResponse.Name,
+			Force:  true,
+		}
+
+		runParams := c.RunParams{
+			VMName:  clonedShowResponse.Name,
+			Command: []string{"diskutil", "apfs", "resizeContainer", "disk1", "0"},
+		}
+
+		gomock.InOrder(
+			client.EXPECT().Exists(config.SourceVMName).Return(true, nil).Times(1),
+			client.EXPECT().Show(config.SourceVMName).Return(sourceShowResponse, nil).Times(1),
+			client.EXPECT().Exists(config.VMName).Return(false, nil).Times(1),
+			client.EXPECT().Clone(cloneParams).Return(nil).Times(1),
+			client.EXPECT().Show(config.VMName).Return(clonedShowResponse, nil).Times(1),
+
+			// modify vm resources
+			client.EXPECT().Stop(stopParams).Return(nil).Times(1),
+			client.EXPECT().Modify(clonedShowResponse.Name, "set", "hard-drive", "-s", config.DiskSize).Return(nil).Times(1),
+			client.EXPECT().Run(runParams).Return(nil, 0).Times(1),
+			client.EXPECT().Stop(stopParams).Return(nil).Times(2),
+			client.EXPECT().Modify(clonedShowResponse.Name, "set", "ram", config.RAMSize).Return(nil).Times(1),
+			client.EXPECT().Stop(stopParams).Return(nil).Times(1),
+			client.EXPECT().Modify(clonedShowResponse.Name, "set", "cpu", "-c", config.CPUCount).Return(nil).Times(1),
+
+			client.EXPECT().Describe(config.VMName).Return(c.DescribeResponse{}, nil).Times(1),
+
+			// modify vm properties
+			client.EXPECT().Stop(stopParams).Return(nil).Times(1),
+			client.EXPECT().Modify(clonedShowResponse.Name, "add", "port-forwarding", "--host-port", strconv.Itoa(config.PortForwardingRules[0].PortForwardingHostPort), "--guest-port", strconv.Itoa(config.PortForwardingRules[0].PortForwardingGuestPort), config.PortForwardingRules[0].PortForwardingRuleName).Return(nil).Times(1),
+			client.EXPECT().Stop(stopParams).Return(nil).Times(1),
+			client.EXPECT().Modify(clonedShowResponse.Name, "set", "custom-variable", "hw.UUID", config.HWUUID).Return(nil).Times(1),
+		)
+
+		mockui := packer.MockUi{}
+		mockui.Say(fmt.Sprintf("Cloning source VM %s into a new virtual machine: %s", config.SourceVMName, config.VMName))
+		mockui.Say(fmt.Sprintf("Modifying VM %s disk size to %s", clonedShowResponse.Name, config.DiskSize))
+		mockui.Say(fmt.Sprintf("Modifying VM %s RAM to %s", clonedShowResponse.Name, config.RAMSize))
+		mockui.Say(fmt.Sprintf("Modifying VM %s CPU core count to %v", clonedShowResponse.Name, config.CPUCount))
+		mockui.Say(fmt.Sprintf("Ensuring %s port-forwarding (Guest Port: %s, Host Port: %s, Rule Name: %s)", clonedShowResponse.Name, strconv.Itoa(config.PortForwardingRules[0].PortForwardingGuestPort), strconv.Itoa(config.PortForwardingRules[0].PortForwardingHostPort), config.PortForwardingRules[0].PortForwardingRuleName))
+		mockui.Say(fmt.Sprintf("Modifying VM custom-variable hw.UUID to %s", config.HWUUID))
+
+		state.Put("vm_name", config.VMName)
+
+		stepAction := step.Run(ctx, state)
+		assert.Equal(t, mockui.SayMessages[0].Message, "Cloning source VM source_foo into a new virtual machine: foo")
+		assert.Equal(t, mockui.SayMessages[1].Message, "Modifying VM foo disk size to 120G")
+		assert.Equal(t, mockui.SayMessages[2].Message, "Modifying VM foo RAM to 16G")
+		assert.Equal(t, mockui.SayMessages[3].Message, "Modifying VM foo CPU core count to 4")
+		assert.Equal(t, mockui.SayMessages[4].Message, "Ensuring foo port-forwarding (Guest Port: 8080, Host Port: 80, Rule Name: rule1)")
+		assert.Equal(t, mockui.SayMessages[5].Message, "Modifying VM custom-variable hw.UUID to abcdefgh")
 		assert.Equal(t, multistep.ActionContinue, stepAction)
 	})
 
